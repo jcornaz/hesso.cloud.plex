399b2f72b359d1a7a6d5ee98995fbe84c4a26ea6
Burgy Benjamin <minidfx@gmail.com>
2015-11-23 21:09:51 +0100

    Use a settings file for reading configuration.

--- webserver/index.py
+++ webserver/index.py
@@ -1,5 +1,12 @@
-import constants
+import yaml
+
 import routes
 
+with open('settings.yaml', 'r') as stream:
+    settings = yaml.load(stream)
+
+if settings is None:
+    raise Exception('Cannot read the settings file.')
+
 if __name__ == '__main__':
-    routes.app.run(constants.__host__, debug = constants.__debug_mode__)
+    routes.app.run(settings['application']['host'], debug = settings['application']['debug'])
