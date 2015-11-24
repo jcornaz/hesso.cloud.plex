45ecd6ed3d0e46edafc386cacb748a0960a70513
Burgy Benjamin <minidfx@gmail.com>
2015-11-24 20:02:53 +0100

    Service for reading information from a filesystem item.

--- webserver/file.py
+++ webserver/file.py
@@ -5,6 +5,8 @@ import flask
 from index import settings
 
 
+# url: https://docs.python.org/2/library/os.path.html
+
 def file_controller(path):
     absolute_path = os.path.abspath('{0}/{1}'.format(settings['application']['storage'], path))
     json = {('path', absolute_path)}
@@ -13,12 +15,14 @@ def file_controller(path):
         json.add(('error', True))
         json.add(('message', 'The given path is not found.'))
 
+        return flask.jsonify(json)
+
     json.add(('exists', True))
     json.add(('name', os.path.basename(absolute_path)))
     json.add(('lastModification', os.path.getmtime(absolute_path)))
 
     if os.path.isdir(absolute_path):
-        json.add(('isDirectory', True))
+        json.add(('isFile', False))
 
     if os.path.isfile(absolute_path):
         json.add(('isFile', True))
