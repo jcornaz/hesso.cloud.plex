7c1ccdd37154aef254b494290df230482032cdb2
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-11-24 17:05:47 +0100

    Container deployed

--- deploy/storage.py
+++ deploy/storage.py
@@ -1,4 +1,4 @@
-from libcloud.storage.types import Provider
+from libcloud.storage.types import Provider, ContainerDoesNotExistError
 from libcloud.storage.providers import get_driver
 
 CONTAINER_NAME = "plex_container"
@@ -7,15 +7,23 @@ CONTAINER_NAME = "plex_container"
 class Storage(object):
     def __init__(self, access_key, secret_key):
         self._driver = get_driver(Provider.S3)(access_key, secret_key)
+        self._container = None
 
     def deploy(self):
-        self._container = self._driver.get_container(CONTAINER_NAME)
-        if not self._container:
-            self._container = self._driver.create_container(CONTAINER_NAME)
+        if self._container is None:
+            try:
+                self._container = self._driver.get_container(CONTAINER_NAME)
+            except ContainerDoesNotExistError:
+                self._container = self._driver.create_container(CONTAINER_NAME)
+
+        print("container deployed")
 
     def destroy(self):
-        if not self._container:
-            self._container = self._driver.get_container()
+        if self._container is None:
+            try:
+                self._container = self._driver.get_container(CONTAINER_NAME)
+            except ContainerDoesNotExistError:
+                self._container = None
 
-        if self._container:
+        if self._container is not None:
             self._driver.delete_container(self._container)
