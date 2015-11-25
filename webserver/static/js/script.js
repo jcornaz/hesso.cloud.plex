c8c549d71b79d16482f007f82f25b62cadedeba3
Burgy Benjamin <minidfx@gmail.com>
2015-11-25 23:16:44 +0100

    Add a new dependency for displaying a treeview using Angular.

--- webserver/static/js/script.js
+++ webserver/static/js/script.js
@@ -1,5 +1,5 @@
 // Module
-var fileManager = angular.module('fileManager', ['ngRoute']);
+var fileManager = angular.module('fileManager', ['ngRoute', 'angularTreeview']);
 
 // configure our routes
 fileManager.config(function($routeProvider) {
@@ -18,13 +18,11 @@ fileManager.config(function($routeProvider) {
     })
 });
 
-// create the controller and inject Angular's $scope
 fileManager.controller('mainController', function($scope) {
 
 
 });
 
-// create the controller and inject Angular's $scope
 fileManager.controller('aboutController', function($scope, $http) {
 
     $http.get('/about').success(function(response)
@@ -32,4 +30,34 @@ fileManager.controller('aboutController', function($scope, $http) {
         $scope.version = response.version;
     });
 
+});
+
+fileManager.controller('treeViewController', function($scope, $http) {
+
+    $http.get('/files').success(function($response)
+    {
+
+    });
+
+    $scope.treedata =
+    [
+        { "label" : "User", "id" : "role1", "children" : [
+            { "label" : "subUser1", "id" : "role11", "children" : [] },
+            { "label" : "subUser2", "id" : "role12", "children" : [
+                { "label" : "subUser2-1", "id" : "role121", "children" : [
+                    { "label" : "subUser2-1-1", "id" : "role1211", "children" : [] },
+                    { "label" : "subUser2-1-2", "id" : "role1212", "children" : [] }
+                ]}
+            ]}
+        ]},
+        { "label" : "Admin", "id" : "role2", "children" : [] },
+        { "label" : "Guest", "id" : "role3", "children" : [] }
+    ];
+
+    $scope.$watch('home_treeview.currentNode', function(newObj, oldObj) {
+        if($scope.home_treeview && angular.isObject($scope.home_treeview.currentNode)) {
+            console.log('Node Selected!!');
+            console.log($scope.home_treeview.currentNode);
+        }
+    }, false);
 });
