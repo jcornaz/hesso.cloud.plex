f349fe50a69f8f4da62f24a7ae9ac04b79da30a7
Burgy Benjamin <minidfx@gmail.com>
2015-11-26 20:13:36 +0100

    Fix the relative path of the selected element.

--- webserver/files.py
+++ webserver/files.py
@@ -9,23 +9,24 @@ from index import settings
 def files_controller():
     storage_path = normcase(normpath(settings['application']['storage']))
 
-    json = {'label': 'storage', 'id': 'root', 'children': browse_folder(storage_path)}
+    json = {'label': 'storage', 'id': '.', 'children': browse_folder(storage_path, storage_path)}
 
     return flask.jsonify(json)
 
 
 # noinspection PyBroadException
-def browse_folder(source_path):
+def browse_folder(source_path, base):
     json = []
 
     try:
         for entry in listdir(source_path):
             path = join(source_path, entry)
+            display_path = remove_base_folder(path, base)
 
-            json_entry = {'label': entry, 'id': path}
+            json_entry = {'label': entry, 'id': display_path}
 
             if isdir(path):
-                json_entry['children'] = browse_folder(path)
+                json_entry['children'] = browse_folder(path, base)
 
             json.append(json_entry)
     except:
