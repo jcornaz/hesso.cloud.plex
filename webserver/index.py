979139c10a2910347751121a2e2d78f61afbea94
Burgy Benjamin <minidfx@gmail.com>
2015-12-05 23:17:14 +0100

    Add and use the port as settings.

--- webserver/index.py
+++ webserver/index.py
@@ -9,8 +9,12 @@ if settings is None:
     raise Exception('Cannot read the settings file.')
 
 if __name__ == '__main__':
+    host = settings['application']['host']
+    debug = settings['application']['debug']
+    port = settings['application']['port']
+
     # Production way
-    routes.app.run(settings['application']['host'], debug = settings['application']['debug'])
+    routes.app.run(host, debug = debug, port = port)
 
     # Development way using livereload
     # server = Server(routes.app.wsgi_app)
