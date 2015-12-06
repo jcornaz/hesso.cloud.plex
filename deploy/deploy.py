8fe63d6c566f74140c08ab5be96db34f464d87bd
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-12-06 17:38:15 +0100

    Add bucket name as an argument

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,5 +1,4 @@
 import sys
-import docker_management as dm
 from storage import Storage
 import yaml
 
@@ -8,13 +7,16 @@ if __name__ == '__main__':
     filename = 'credentials.yml'
     keypair = 'keypair.pem'
 
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
+    if len(sys.argv) <= 1:
+        print('You must specify a S3 bucket name')
+    else:
+        bucketname = sys.argv[1]
         if len(sys.argv) > 2:
-            keypair = sys.argv[2]
+            filename = sys.argv[1]
+        else:
+            filename = 'credentials.yml'
 
     with open(filename) as file:
         credentials = yaml.load(file)
 
-    Storage(credentials['id'], credentials['key']).deploy()
-    dm.deploy(keypair, credentials['id'], credentials['key'])
+    Storage(bucketname, credentials['id'], credentials['key']).deploy()
